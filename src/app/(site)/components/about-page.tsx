d2d86ba03da00021e12df940d4eaabf07378f779
Braedne <braedenltd@gmail.com>
2023-12-19 15:21:56 +1300

    Fixes

--- src/app/(site)/components/about-page.tsx
+++ src/app/(site)/components/about-page.tsx
@@ -5,10 +5,10 @@ import Photo from "../assets/images/35ac116a8b1e821cb1bf3bd1e004e6a4-cover-large
 import CountUp from "react-countup";
 import CountScrollTrigger from "react-scroll-trigger";
 import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
 } from "../components/ui/accordion";
 import { useEffect, useRef, useState } from "react";
 import AboutAccordions from "./about-accordions";
@@ -17,201 +17,201 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function AboutSection({ info }: any) {
-	const [countersOn, setCountersOn] = useState([false, false, false]);
+  const [countersOn, setCountersOn] = useState([false, false, false]);
 
-	const [isInView, setIsInView] = useState(false);
-	const sectionRef = useRef(null);
+  const [isInView, setIsInView] = useState(false);
+  const sectionRef = useRef(null);
 
-	
-	useEffect(() => {
-		gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
 
-		const observer = new IntersectionObserver(
-			([entry]) => {
-				console.log(entry.isIntersecting);
-				setIsInView(entry.isIntersecting);
-			},
-			{ threshold: 0 }
-		);
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        console.log(entry.isIntersecting);
+        setIsInView(entry.isIntersecting);
+      },
+      { threshold: 0 }
+    );
 
-		if (sectionRef.current) {
-			observer.observe(sectionRef.current);
-		}
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
 
-		const experience = gsap.timeline({
-			scrollTrigger: {
-				trigger: ".numbers",
-				pin: true,
-				start: "top-=100 top",
-				end: "bottom+=620 top",
-				scrub: 1,
-				anticipatePin: 1,
-				pinSpacing: true,
-			},
-		});
+    const experience = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".numbers",
+        pin: true,
+        start: "top-=100 top",
+        end: "bottom+=620 top",
+        scrub: 1,
+        anticipatePin: 1,
+        pinSpacing: true,
+      },
+    });
 
-		const awards = gsap.timeline({
-			scrollTrigger: {
-				trigger: ".awards",
-				pin: true,
-				start: "top-=400 top",
-				end: "bottom-=675 top",
-				scrub: 1,
-				anticipatePin: 1,
-				pinSpacing: true,
-			},
-		});
+    const awards = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".awards",
+        pin: true,
+        start: "top-=400 top",
+        end: "bottom-=675 top",
+        scrub: 1,
+        anticipatePin: 1,
+        pinSpacing: true,
+      },
+    });
 
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
 
-		return () => {
-			if (sectionRef.current) {
-				observer.unobserve(sectionRef.current);
-			}
+      awards.kill();
+      experience.kill();
+    };
+  }, []);
 
-			awards.kill();
-			experience.kill();
-		};
-	}, []);
+  const handleEnterViewport = (index: number) => {
+    setCountersOn((prev) => {
+      const newState = [...prev];
+      newState[index] = true;
+      return newState;
+    });
+  };
 
-	const handleEnterViewport = (index: number) => {
-		setCountersOn((prev) => {
-			const newState = [...prev];
-			newState[index] = true;
-			return newState;
-		});
-	};
+  return (
+    <section className={`col-span-12`}>
+      <div className="grid grid-cols-2 min-h-screen relative">
+        <div className="bg-black md:sticky top-0 h-screen overflow-hidden col-span-2 md:col-span-1 hidden md:block">
+          <Image
+            className="object-cover"
+            src={Photo}
+            alt="Richard and Sam"
+            width={2000}
+            height={1000}
+            priority
+          />
+        </div>
+        <div className=" mt-6 col-span-2 md:col-span-1">
+          <div className="bg-white  md:min-h-screen mx-[10px] md:mx-5">
+            <h1 className="font-medium text-sm-xl md:text-xl col-span-11 mb-medium  md:col-span-1 mt-16 md:mt-0">
+              About Us
+            </h1>
+            <div className="text-xs-medium md:text-base mb-5 md:mb-[50vh]">
+              {info.description}
+            </div>
+            <div className="text-xxs font-medium md:text-xs-medium my-3 md:my-7 pt-6 ">
+              Over the Years
+            </div>
+            <div className="text-xxs md:text-xs mb-[48px]  whitespace-pre-line">
+              {info.over_the_years}
+            </div>
+            <div className="text-xxs md:text-xs flex justify-between mb-medium md:mb-large">
+              <div className="me-5 md:me-0 font-medium">Magazine</div>
+              <div className="inline-block">
+                Want to learn more about us? Check out our magazine
+                <a target="_blank" href="" className="inline-block ms-2">
+                  {" "}
+                  here{" "}
+                  <span className="inline-block">
+                    <svg
+                      width="6"
+                      height="12"
+                      viewBox="0 0 6 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M5.65685 5.65685L0 0L0.00012207 11.3138L5.65685 5.65685Z"
+                        fill="#999999"
+                      />
+                    </svg>
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
 
-	return (
-		<section className={`col-span-12`}>
-			<div className="grid grid-cols-2 min-h-screen relative">
-				<div className="bg-black md:sticky top-0 h-screen overflow-hidden col-span-2 md:col-span-1 hidden md:block">
-					<Image
-						className="object-cover"
-						src={Photo}
-						alt="Richard and Sam"
-						width={2000}
-						height={1000}
-						priority
-					/>
-				</div>
-				<div className=" mt-6 col-span-2 md:col-span-1">
-					<div className="bg-white  md:min-h-screen mx-[10px] md:mx-5">
-						<h1 className="font-medium text-sm-xl md:text-xl col-span-11 mb-medium  md:col-span-1 mt-16 md:mt-0">
-							About Us
-						</h1>
-						<div className="text-xs-medium md:text-base mb-5 md:mb-[50vh]">
-							{info.description}
-						</div>
-						<div className="text-xxs font-medium md:text-xs-medium my-3 md:my-7 pt-6 ">
-							Over the Years
-						</div>
-						<div className="text-xxs md:text-xs mb-[48px]  whitespace-pre-line">
-							{info.over_the_years}
-						</div>
-						<div className="text-xxs md:text-xs flex justify-between mb-medium md:mb-large">
-							<div className="me-5 md:me-0 font-medium">Magazine</div>
-							<div className="inline-block">
-								Want to learn more about us? Check out our magazine
-								<a target="_blank" href="" className="inline-block ms-2">
-									{" "}
-									here{" "}
-									<span className="inline-block">
-										<svg
-											width="6"
-											height="12"
-											viewBox="0 0 6 12"
-											fill="none"
-											xmlns="http://www.w3.org/2000/svg"
-										>
-											<path
-												d="M5.65685 5.65685L0 0L0.00012207 11.3138L5.65685 5.65685Z"
-												fill="#999999"
-											/>
-										</svg>
-									</span>
-								</a>
-							</div>
-						</div>
-					</div>
+          <Image
+            src={Photo}
+            alt="Richard and Sam"
+            width={2000}
+            height={1000}
+            priority
+            className="block md:hidden mb-20 md:mb-32"
+          />
 
-					<Image
-						src={Photo}
-						alt="Richard and Sam"
-						width={2000}
-						height={1000}
-						priority
-						className="block md:hidden mb-20 md:mb-32"
-					/>
+          <div
+            className={`numbers md:h-screen bg-white z-[2] border-t md:border-b border-dotted border-grey pt-3 pb-32 md:pb-24 mx-[10px] md:mx-5 col-span-2  md:col-span-1 mt-12`}
+          >
+            <h4 className="pt-3 text-xs-medium">Years of Experience</h4>
+            <CountScrollTrigger onEnter={() => handleEnterViewport(0)}>
+              {countersOn[0] && (
+                <CountUp
+                  start={0}
+                  end={25}
+                  duration={1}
+                  suffix="+"
+                  className="font-medium text-sm-2xl md:text-2xl mb-24"
+                />
+              )}
+            </CountScrollTrigger>
+          </div>
 
-					<div
-						className={`numbers md:h-screen bg-white z-[2] border-t md:border-b border-dotted border-grey pt-3 pb-32 md:pb-24 mx-[10px] md:mx-5 col-span-2  md:col-span-1 mt-12`}
-					>
-						<h4 className="pt-3 text-xs-medium">Years of Experience</h4>
-						<CountScrollTrigger onEnter={() => handleEnterViewport(0)}>
-							{countersOn[0] && (
-								<CountUp
-									start={0}
-									end={25}
-									duration={1}
-									suffix="+"
-									className="font-medium text-sm-2xl md:text-2xl mb-24"
-								/>
-							)}
-						</CountScrollTrigger>
-					</div>
+          <div
+            className={`awards md:h-screen z-[3] relative bg-white border-t border-dotted border-grey pt-3 pb-32 md:pb-24 mx-[10px] md:mx-5 col-span-2 md:col-span-1 ${
+              isInView ? "" : ""
+            } `}
+          >
+            <h4 className="pt-3 text-xs-medium">Master Builders Awards</h4>
+            <CountScrollTrigger onEnter={() => handleEnterViewport(1)}>
+              {countersOn[1] && (
+                <CountUp
+                  start={0}
+                  end={20}
+                  duration={0.75}
+                  suffix="+"
+                  className="font-medium text-sm-2xl md:text-2xl mb-24"
+                />
+              )}
+            </CountScrollTrigger>
+          </div>
 
-					<div className={`awards md:h-screen z-[3] relative bg-white border-t border-dotted border-grey pt-3 pb-32 md:pb-24 mx-[10px] md:mx-5 col-span-2 md:col-span-1 ${
-							isInView ? "" : ""
-						} `}>
-						<h4 className="pt-3 text-xs-medium">Master Builders Awards</h4>
-						<CountScrollTrigger onEnter={() => handleEnterViewport(1)}>
-							{countersOn[1] && (
-								<CountUp
-									start={0}
-									end={20}
-									duration={0.75}
-									suffix="+"
-									className="font-medium text-sm-2xl md:text-2xl mb-24"
-								/>
-							)}
-						</CountScrollTrigger>
-					</div>
+          <div
+            className={`relative z-[5] border-t border-dotted border-grey pt-3 pb-24  mx-[10px] md:mx-5 fade-in-section bg-white`}
+          >
+            <h4 className="pt-3 text-xs-medium">Houses Built</h4>
+            <CountScrollTrigger onEnter={() => handleEnterViewport(2)}>
+              {countersOn[2] && (
+                <CountUp
+                  start={0}
+                  end={1500}
+                  duration={0.5}
+                  separator=""
+                  suffix="+"
+                  className="font-medium text-sm-2xl md:text-2xl mb-24"
+                />
+              )}
+            </CountScrollTrigger>
+            <div
+              className={`sticky top-0  py-32 md:mb-0 px-[10px] md:px-5 fade-in-section bg-white `}
+            >
+              <AboutAccordions />
+            </div>
+          </div>
 
-					<div
-						className={`relative z-[5] border-t border-dotted border-grey pt-3 pb-24  mx-[10px] md:mx-5 fade-in-section bg-white`}
-					>
-						<h4 className="pt-3 text-xs-medium">Houses Built</h4>
-						<CountScrollTrigger onEnter={() => handleEnterViewport(2)}>
-							{countersOn[2] && (
-								<CountUp
-									start={0}
-									end={1500}
-									duration={0.5}
-									separator=""
-									suffix="+"
-									className="font-medium text-sm-2xl md:text-2xl mb-24"
-								/>
-							)}
-						</CountScrollTrigger>
-						<div
-							className={`sticky top-0  py-32 md:mb-0 px-[10px] md:px-5 fade-in-section bg-white `}
-						>
-							<AboutAccordions />
-						</div>
-					</div>
-
-					<div
-						ref={sectionRef}
-						className={`sticky top-0 z-[7] md:h-min-screen fade-in-section pt-[50px] ${
-							isInView ? "bg-black text-white" : "bg-white"
-						}`}
-					>
-						<AboutTeam info={info} />
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+          <div
+            ref={sectionRef}
+            className={`sticky top-0 z-[7] md:h-min-screen fade-in-section pt-[50px] ${
+              isInView ? "bg-black text-white" : "bg-white"
+            }`}
+          >
+            <AboutTeam info={info} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default AboutSection;
--- src/app/(site)/components/upcoming_project_list.tsx
+++ src/app/(site)/components/upcoming_project_list.tsx
@@ -1,70 +1,146 @@
 "use client";
 
-import { X } from "lucide-react";
+import { X, ArrowLeft, ArrowRight } from "lucide-react";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 interface ProjectImage {
-	imageUrl: string;
+  imageUrl: string;
 }
 
 interface Project {
-	hero_image: ProjectImage;
-	featured_image_1: ProjectImage;
-	featured_image_2: ProjectImage;
-	images: ProjectImage[];
-	suburb: string;
+  hero_image: ProjectImage;
+  featured_image_1: ProjectImage;
+  featured_image_2: ProjectImage;
+  images: ProjectImage[];
+  suburb: string;
 }
+
 export default function UpcomingProjectsList({ projects }: any) {
-	return (
-		<div className="col-span-12">
-			{projects.map((upcomingProject: Project, index: number) => (
-				<div
-					key={index}
-					className="border-grey border-dashed border-t border-1 mb-[60px] grid grid-cols-6 md:grid-cols-12 gap-x-5 col-span-12"
-				>
-					<div className="col-span-3 md:col-span-2 mt-[10px] mb-9">Suburb</div>
-					<div className="col-span-3 md:col-span-10 mt-[10px] md:col-start-3">
-						{upcomingProject.suburb}
-					</div>
+  const [lightboxOpen, setLightboxOpen] = useState(false);
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+  const [imageIndex, setImageIndex] = useState(0);
 
-					<Image
-						alt=""
-						src={upcomingProject.featured_image_1.imageUrl}
-						className="cursor-pointer upcoming-project order-2 md:order-1 my-3 md:my-0 md:row-start-2 col-span-3 md:col-span-2 col-start-4 md:col-start-1 w-full"
-						width={1000}
-						height={1000}
-					/>
-					<Image
-						alt=""
-						src={upcomingProject.featured_image_2.imageUrl}
-						className="cursor-pointer upcoming-project order-3 md:order-2 my-3 md:my-0 md:row-start-2 col-span-3 md:col-span-2 col-start-4 md:col-start-3  w-full"
-						width={1000}
-						height={1000}
-					/>
+  const openLightbox = (project: Project, index: number) => {
+    setSelectedProject(project);
+    setImageIndex(index);
+    setLightboxOpen(true);
+  };
 
-					<Image
-						alt=""
-						src={upcomingProject.hero_image.imageUrl}
-						className="cursor-pointer upcoming-project col-span-6 md:col-start-7
-						"
-						width={1000}
-						height={1000}
-					/>
+  const closeLightbox = () => {
+    setLightboxOpen(false);
+    setSelectedProject(null);
+    setImageIndex(0);
+  };
 
-					<div
-						className="hidden fixed top-0 left-0 w-full h-screen bg-opacity-20 backdrop-blur-2xl"
-						id="lightbox"
-					>
-						<button className="flex items-center fixed top-2 right-[10px] md:right-5 text-xxs bg-white bg-opacity-20 backdrop-blur-lg  p-[10px] rounded-[5px] z-[50] text-black transition duration-500 hover:bg-opacity-50">
-							Close{" "}
-							<div className="ml-12">
-								<X size={15} />
-							</div>
-						</button>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+  const nextImage = () => {
+    if (selectedProject) {
+      setImageIndex((prevIndex) => (prevIndex + 1) % 3);
+    }
+  };
+
+  const previousImage = () => {
+    if (selectedProject) {
+      setImageIndex((prevIndex) => (prevIndex - 1 + 3) % 3);
+    }
+  };
+
+  const getImageUrl = (project: Project, index: number) => {
+    return [
+      project.featured_image_1.imageUrl,
+      project.featured_image_2.imageUrl,
+      project.hero_image.imageUrl,
+    ][index];
+  };
+
+  const Dots = ({ numImages, currentIndex }: any) => (
+    <div className="absolute top-4 left-4 flex space-x-2">
+      {[...Array(numImages)].map((_, index) => (
+        <div
+          key={index}
+          className={`h-2 w-2 rounded-full ${
+            currentIndex === index ? "bg-black" : "bg-white"
+          }`}
+        />
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="col-span-12">
+      {projects.map((upcomingProject: Project, index: number) => (
+        <div
+          key={index}
+          className="border-grey border-dashed border-t border-1 mb-[60px] grid grid-cols-6 md:grid-cols-12 gap-x-5 col-span-12"
+        >
+          <div className="col-span-3 md:col-span-2 mt-[10px] mb-9">
+            Suburbtest
+          </div>
+          <div className="col-span-3 md:col-span-10 mt-[10px] md:col-start-3">
+            {upcomingProject.suburb}
+          </div>
+
+          <Image
+            alt=""
+            src={upcomingProject.featured_image_1.imageUrl}
+            className="cursor-pointer upcoming-project order-2 md:order-1 my-3 md:my-0 md:row-start-2 col-span-3 md:col-span-2 col-start-4 md:col-start-1 w-full"
+            width={1000}
+            height={1000}
+            onClick={() => openLightbox(upcomingProject, 0)}
+          />
+          <Image
+            alt=""
+            src={upcomingProject.featured_image_2.imageUrl}
+            className="cursor-pointer upcoming-project order-3 md:order-2 my-3 md:my-0 md:row-start-2 col-span-3 md:col-span-2 col-start-4 md:col-start-3  w-full"
+            width={1000}
+            height={1000}
+            onClick={() => openLightbox(upcomingProject, 1)}
+          />
+
+          <Image
+            alt=""
+            src={upcomingProject.hero_image.imageUrl}
+            className="cursor-pointer upcoming-project col-span-6 md:col-start-7"
+            width={1000}
+            height={1000}
+            onClick={() => openLightbox(upcomingProject, 2)}
+          />
+
+          {lightboxOpen && selectedProject && (
+            <div className="fixed top-0 left-0 w-full h-screen bg-opacity-80 backdrop-blur-2xl z-50 flex justify-center items-center">
+              <Dots numImages={3} currentIndex={imageIndex} />
+
+              <button
+                className="absolute bottom-2 right-5 z-[50] text-white cursor-pointer"
+                onClick={closeLightbox}
+              >
+                <X size={30} />
+              </button>
+
+              <button
+                className="absolute top-[50%] left-5  z-[50] text-white cursor-pointer"
+                onClick={previousImage}
+              >
+                <ArrowLeft size={30} />
+              </button>
+
+              <button
+                className="absolute top-[50%] right-5  z-[50] text-white cursor-pointer"
+                onClick={nextImage}
+              >
+                <ArrowRight size={30} />
+              </button>
+
+              <Image
+                alt="Selected"
+                src={getImageUrl(selectedProject, imageIndex)}
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
