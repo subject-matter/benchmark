711e08fac103e661f19b3bd4b0c7ac175b56df99
Rishi Samuel Patel <rishisamuelpatel@gmail.com>
2024-03-07 10:37:28 +1300

    deploy

--- src/app/(site)/components/header.tsx
+++ src/app/(site)/components/header.tsx
@@ -10,45 +10,44 @@ import { Plus } from "lucide-react";
 export default function Header() {
 	
 	const pathname = usePathname();
-	// @ts-ignore
-	const isOurProcessPage =
-		pathname === "/our-process" && window.innerWidth <= 767;
+
 
 	useEffect(() => {
-		const openMenu = () => {
-			const menu = document.getElementById("menu");
-			const glass = document.getElementById("glass");
-			const body = document.body;
-			if (menu && glass) {
-				menu.classList.add("open");
-				glass.style.opacity = "100";
-				glass.style.visibility = "visible";
-				body.classList.add("overflow-y-hidden");
-			}
-		};
-		const button = document.getElementById("openMenuButton");
-		if (button) {
-			button.addEventListener("click", openMenu);
-		}
-	}, []);
+    const openMenu = () => {
+      const menu = document.getElementById('menu');
+      const glass = document.getElementById('glass');
+      const body = document.body;
+      if (menu && glass) {
+        menu.classList.add('open');
+        glass.style.opacity = '100';
+        glass.style.visibility = 'visible';
+        body.classList.add('overflow-y-hidden');
+      }
+    };
+
+    const button = document.getElementById('openMenuButton');
+    if (button) {
+      button.addEventListener('click', openMenu);
+    }
+  }, []);
 
 	return (
-		<header className="" id="header">
-			<Container>
-				<div className="flex flex-wrap">
-					<nav className="ml-auto flex space-x-3 w-full text-sm md:text-base md:w-auto">
-						<button
-							id={"openMenuButton"}
-							className={`w-[100px] h-[30px] md:h-[35px] md:w-[120px] fixed top-[10px] right-[10px] md:right-5 text-xxs bg-[#999999] bg-opacity-10 px-3 rounded-[5px] z-20 backdrop-blur-lg flex justify-between items-center ${isOurProcessPage ? "text-white" : "text-black"} `}
-						>
-							Menu{" "}
-							<span>
-								<Plus size={15} />
-							</span>
-						</button>
-					</nav>
-				</div>
-			</Container>
-		</header>
-	);
+    <header className="" id="header">
+      <Container>
+        <div className="flex flex-wrap">
+          <nav className="ml-auto flex w-full space-x-3 text-sm md:w-auto md:text-base">
+            <button
+              id={'openMenuButton'}
+              className={`fixed right-[10px] top-[10px] z-20 flex h-[30px] w-[100px] items-center justify-between rounded-[5px] bg-[#999999] bg-opacity-10 px-3 text-xxs backdrop-blur-lg md:right-5 md:h-[35px] md:w-[120px] gi `}
+            >
+              Menu{' '}
+              <span>
+                <Plus size={15} />
+              </span>
+            </button>
+          </nav>
+        </div>
+      </Container>
+    </header>
+  );
 }
