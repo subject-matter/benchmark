bf82c88d247f84009b4a9135f998406cbd24a340
Braedne <braedenltd@gmail.com>
2024-04-11 18:11:49 +1000

    fix

--- src/app/defaultDocumentNode.js
+++ src/app/defaultDocumentNode.js
@@ -3,12 +3,13 @@ import { Iframe } from "sanity-plugin-iframe-pane";
 // Customise this function to show the correct URL based on the current document
 function getPreviewUrl(doc) {
   return doc?.slug?.current
-    ? `${process.env.NEXT_PUBLIC_APP_URL}/projects/${doc.slug.current}`
-    : `${process.env.NEXT_PUBLIC_APP_URL}/project`;
+    ? `${window.location.origin}/projects/${doc.slug.current}`
+    : `${window.location.origin}/project`;
 }
 
 // Import this into the deskTool() plugin
-export const defaultDocumentNode = (S) => {
+export const defaultDocumentNode = (S, data) => {
+  console.log(data);
   return S.document().views([
     S.view.form(),
     S.view
