a79c075c876aba07e71381da4bc3774147b3e7be
Rishi Patel <rishisamuelpatel@gmail.com>
2024-09-29 09:57:31 +0100

    hide fields

--- sanity/schemas/post.js
+++ sanity/schemas/post.js
@@ -69,6 +69,7 @@ export const post = {
           type: "articleSection",
         }),
       ],
+      hidden: true,
     }),
     {
       name: "body",
@@ -82,6 +83,7 @@ export const post = {
           type: "image",
         },
       ],
+      hidden: true,
     },
     {
       title: "SEO / Share Settings",
